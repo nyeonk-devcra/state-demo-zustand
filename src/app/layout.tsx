c741619f55b62d0c810236f18da760a9ce9b942d
riky <riky.kim@devcra.com>
2024-10-11 13:12:32 +0900

    adjust webfont

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import { pretendardStd } from "@/components/fonts";
+import clsx from "clsx";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={pretendardStd.className}>{children}</body>
+      <body className={clsx("font-pretendardStd", pretendardStd.variable)}>
+        {children}
+      </body>
     </html>
   );
 }
